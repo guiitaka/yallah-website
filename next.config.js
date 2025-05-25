e52327cfd90d04bddf1b74610bcda10b63e10f54
guiitaka <guilherme.takahashi@icloud.com>
2025-05-25 12:03:30 -0300

    fix: correct outputFileTracing option in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        outputFileTracing: false,
-    },
+    outputFileTracing: false,
     images: {
         domains: [
             'images.unsplash.com',
