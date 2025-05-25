ecc03997f8369e3568ee35c917aacf716527c2ad
guiitaka <guilherme.takahashi@icloud.com>
2025-05-25 02:16:39 -0300

    Refatora cliente Supabase para usar variáveis de ambiente e corrige rota de verificação de sessão

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://uzdybyxqwaahdjaaybet.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6ZHlieXhxd2FhaGRqYWF5YmV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczMzE4NzQsImV4cCI6MjA2MjkwNzg3NH0.NpjlrfC2-izZKFjyUWC4RlTJYgUL9reqyzearSIsPYs';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
