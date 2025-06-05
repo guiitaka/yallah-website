6bf958bfdd01a0dccb703a7d600a61f2c0b2dc19
guiitaka <guilherme.takahashi@icloud.com>
2025-06-04 23:54:09 -0300

    Fix barrel file imports with explicit file extensions

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,7 @@
-export { Button, buttonVariants } from "./button";
-export { Popover, PopoverContent, PopoverTrigger } from "./popover";
-export { Label } from "./label";
-export { Checkbox } from "./checkbox";
-export { Slider } from "./slider";
-export { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./select";
-export { Input } from "./input"; 
+export { Button, buttonVariants } from "./button.tsx";
+export { Popover, PopoverContent, PopoverTrigger } from "./popover.tsx";
+export { Label } from "./label.tsx";
+export { Checkbox } from "./checkbox.tsx";
+export { Slider } from "./slider.tsx";
+export { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./select.tsx";
+export { Input } from "./input.tsx"; 
